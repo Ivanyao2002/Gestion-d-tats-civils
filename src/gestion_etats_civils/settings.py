675814a0da0557d30e7355cbeaa426a43905d4d8
PaulEkra <99322544+PaulEkra@users.noreply.github.com>
2024-07-11 10:35:43 +0000

    10

--- src/gestion_etats_civils/settings.py
+++ src/gestion_etats_civils/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mairie_db',
         'USER': 'postgres',
-        'PASSWORD': 'ivan',
+        'PASSWORD': 'Pipi1306',
         'HOST': 'localhost',
         'PORT': '5432',
     }
